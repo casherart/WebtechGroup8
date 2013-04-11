11f19b3440b0afca9506c5d2854279b5b6247698
DuyThaiNguyen <DuyThaiNguyen@noname>
2013-04-11 11:28:05 +0200

    updated hit counter

--- site/footer.php
+++ site/footer.php
@@ -42,8 +42,8 @@
 
 <!-- Hit Counter -->
 <?php
-$hit_count = @file_get_contents('count.txt');
-echo $hit_count;
-$hit_count++;
-@file_put_contents('count.txt', $hit_count);
+    $hit_count = @file_get_contents('count.txt');
+    echo $hit_count;
+    $hit_count++;
+    @file_put_contents('count.txt', $hit_count);
 ?>
