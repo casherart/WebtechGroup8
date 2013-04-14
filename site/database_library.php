3c3f9d7394a492ac8aace2b1b7df3521a94b9cfb
DuyThaiNguyen <DuyThaiNguyen@noname>
2013-04-14 11:44:00 +0200

    updated database functions

--- site/database_library.php
+++ site/database_library.php
@@ -2,8 +2,7 @@
 
 // get select options for form
 function get_select_options($host, $username, $password, $database_name, $query) {
-    $con = mysql_connect($host, $username, $password) or die("Connection Error: " . mysql_error());
-    mysql_select_db($database_name, $con);
+    connect_database($host, $username, $password, $database_name);
     $result = mysql_query($query);
     while ($row = mysql_fetch_array($result)) {
         echo "<option value=\"" . $row[0] . "\">" . $row[1] . "</option>";
