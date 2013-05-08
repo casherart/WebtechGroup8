f3f000d89ef43e361a8028105dffc64d69a4593c
DuyThaiNguyen <DuyThaiNguyen@noname>
2013-05-08 09:53:40 +0200

    fixed path

--- site/_include/navigation_app.php
+++ site/_include/navigation_app.php
@@ -5,10 +5,10 @@
 	<div class="navbar-inner">
 		<div class="container">		
 			<ul class="nav">
-				<li <?php if ($filename == "app_map") echo("class='active'"); ?>><a href="app_map.php">Map</a></li>
-				<li <?php if ($filename == "app_boatinfo") echo("class='active'"); ?>><a href="app_boatinfo.php">Logbuch</a></li>
-				<li <?php if ($filename == "app_trip" || $filename == "app_tripinfo") echo("class='active'"); ?>><a href="app_trip.php">Routen</a></li>
-                                <li <?php if ($filename == "app_weather") echo("class='active'"); ?>><a href='app_weather.php'>Wetter</a></li>
+				<li <?php if ($filename == "app_map") echo("class='active'"); ?>><a href="/site/app_map.php">Map</a></li>
+				<li <?php if ($filename == "app_boatinfo") echo("class='active'"); ?>><a href="/site/app_boatinfo.php">Logbuch</a></li>
+				<li <?php if ($filename == "app_trip" || $filename == "app_tripinfo") echo("class='active'"); ?>><a href="/site/app_trip.php">Routen</a></li>
+                                <li <?php if ($filename == "app_weather") echo("class='active'"); ?>><a href='/site/app_weather.php'>Wetter</a></li>
 			</ul>
 			<ul class="navbar-form pull-right" style="list-style-type: none;">
 				<li><a class="btn" id="startSimulation" title="start Simulation"><i class="icon-play"></i></a><li>
