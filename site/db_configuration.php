5fb407b2670337ebfd0e48825c8cebd0c1dbc744
DuyThaiNguyen <DuyThaiNguyen@noname>
2013-05-09 20:55:47 +0200

    updated db automatic selection

--- site/db_configuration.php
+++ site/db_configuration.php
@@ -6,20 +6,20 @@
 // Damit alle Fehler angezeigt werden
 error_reporting(E_ALL);
 
-if ($_SERVER['HTTP_HOST'] == "localhost:8888") 
+if ($_SERVER['HTTP_HOST'] == "seapal.dnguyen.de" || $_SERVER['HTTP_HOST'] == "dnguyen.de") 
+{
+    define ('MYSQL_HOST','rdbms.strato.de');
+    define ('MYSQL_USER','U1323276');
+    define ('MYSQL_PW','seapal2711');
+    define ('MYSQL_DB','DB1323276');
+} 
+else
 {
     // local
     define ('MYSQL_HOST','localhost');
     define ('MYSQL_USER','root');
     define ('MYSQL_PW','root');
     define ('MYSQL_DB','seapal');
-} 
-else
-{
-    define ('MYSQL_HOST','rdbms.strato.de');
-    define ('MYSQL_USER','U1323276');
-    define ('MYSQL_PW','seapal2711');
-    define ('MYSQL_DB','DB1323276');
 }
 ?>
 
