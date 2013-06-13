c7c4ec2adffb32da006d2e80ab28da8f4f258838
DuyThaiNguyen <DuyThaiNguyen@noname>
2013-06-13 10:33:00 +0200

    updated style weatherbox

--- site/app_map.php
+++ site/app_map.php
@@ -66,25 +66,25 @@
             <div id="weatherDisplayBox" class="well well-small btn-inverse disabled container" style="display: none;">
                 <div id="navDisplayBox" data-toggle="buttons-radio" class="btn-group row-fluid">
 	                <div class="span12">
-	                    <button id="now" class="btn btn-info span1 active" style="min-width:69px;margin:0px;padding:0px;">Aktuell</button>
-	                    <button id="today" class="btn btn-info span1" style="min-width:69px;margin:0px;padding:0px;">Heute</button>
-	                    <button id="tomorrow" class="btn btn-info span1" style="min-width:69px;margin:0px;padding:0px;">Morgen</button>
-	                    <button id="3days" class="btn btn-info span1" style="min-width:69px;margin:0px;padding:0px;">3 Tage</button>
-	                    <button id="7days" class="btn btn-info span1" style="min-width:69px;margin:0px;padding:0px;">7 Tage</button>
+	                    <button id="now" class="btn btn-info span1 active weatherButton">Aktuell</button>
+	                    <button id="today" class="btn btn-info span1 weatherButton">Heute</button>
+	                    <button id="tomorrow" class="btn btn-info span1 weatherButton">Morgen</button>
+	                    <button id="3days" class="btn btn-info span1 weatherButton">3 Tage</button>
+	                    <button id="7days" class="btn btn-info span1 weatherButton">7 Tage</button>
 	                </div>
                 </div>
                 <div class="clearfix"></div>
                 <a id ="detail" style="" href="#">
                     <div id="weatherDisplayTop">
-                        <div align="center" style="width: 140px; height: 80px; float: left;">
+                        <div id="weatherTopTemp" align="center">
                             <div id="tempDataMax" class="data" style=""></div>
                             <div id="nameData" class="data" style="">
-                            	<img src="/img/icons/ajax-loader-black.gif">
+                            <img id="weatherLoader" src="/img/icons/ajax-loader-black.gif">
                             </div>
                             <div id="tempDataMin" class="data" style=""></div>
                         </div>
                         <div id="tempData" class="data" style=""></div>
-                        <div id="time" class="data" style="padding-top: 18px; text-align: right; float: right;"></div>
+                        <div id="time" class="data"></div>
                     </div>
                     <div class="clearfix"></div>
                     <div id="weatherDisplayBottom">
