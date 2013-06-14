7a87180bed81c4c4fdfeeda851965edc55b8d6f2
raphidue <nguyenthaiduy90@googlemail.com>
2013-06-14 13:03:34 +0200

    some fixes

--- Seapal/app/controllers/Map.java
+++ Seapal/app/controllers/Map.java
@@ -12,11 +12,6 @@ import views.html.*;
 import views.html._include.*;
 import java.util.List;
 import java.util.ArrayList;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import java.util.Random;
 import java.util.GregorianCalendar;
 
